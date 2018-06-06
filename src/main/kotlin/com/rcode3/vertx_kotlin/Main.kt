f1692b5aefdab28cb67f93472abd740d008dd53c
andy <andy@arin.net>
2018-06-06 04:24:06 -0700

    refactored to use Main verticle

--- src/main/kotlin/com/rcode3/vertx_kotlin/Main.kt
+++ src/main/kotlin/com/rcode3/vertx_kotlin/Main.kt
@@ -11,9 +11,13 @@ import io.vertx.core.Vertx
  * This is an example of starting vertx and deploying verticles from a typical main method.
  *
  * To do this, the jar must have this class listed in the manifest as Main-Class
+ *
+ * PLEASE NOTE: this is only provided to show what is possible. It does not do everything that
+ * [com.rcode3.vertx_kotlin.verticles.Main] does.
  */
 
 fun main( args: Array<String> ) {
+
     println( "hello world" )
     val vertx = Vertx.vertx()
     vertx.deployVerticle( SimpleVerticle::class.java.name ) { handleVerticleDeployment( it ) }
@@ -22,6 +26,8 @@ fun main( args: Array<String> ) {
     vertx.setPeriodic( 2000 ) {
         vertx.eventBus().publish( PERIODIC_TIMER_ADDR, null )
     }
+
+    // just to show what is possible
 }
 
 fun handleVerticleDeployment(result: AsyncResult<String> ) {
--- src/main/kotlin/com/rcode3/vertx_kotlin/verticles/Main.kt
+++ src/main/kotlin/com/rcode3/vertx_kotlin/verticles/Main.kt
@@ -1,14 +1,40 @@
 // Copyright (C) 2018 Andrew Newton
 package com.rcode3.vertx_kotlin.verticles
 
+import com.rcode3.vertx_kotlin.PERIODIC_TIMER_ADDR
+import com.rcode3.vertx_kotlin.verticles.example.ExampleJSONReceiver
+import com.rcode3.vertx_kotlin.verticles.example.ExampleJSONSender
+import com.rcode3.vertx_kotlin.verticles.example.SimpleVerticle
 import io.vertx.core.AbstractVerticle
+import io.vertx.core.AsyncResult
 import io.vertx.core.Future
 
+/**
+ * This is a "Main" verticle. It is used to deploy all other verticles.
+ * This verticle is deployed by the Vertx [io.vertx.core.Launcher] class.
+ */
 class Main : AbstractVerticle() {
 
     override fun start(startFuture: Future<Void>) {
 
+        println( "hello world" )
+        vertx.deployVerticle( SimpleVerticle::class.java.name ) { handleVerticleDeployment( it ) }
+        vertx.deployVerticle( ExampleJSONReceiver::class.java.name ) { handleVerticleDeployment( it ) }
+        vertx.deployVerticle( ExampleJSONSender::class.java.name ) { handleVerticleDeployment( it ) }
+        vertx.setPeriodic( 2000 ) {
+            vertx.eventBus().publish( PERIODIC_TIMER_ADDR, null )
+        }
+
         startFuture.complete()
     }
 
+    fun handleVerticleDeployment(result: AsyncResult<String>) {
+        if( result.succeeded() ) {
+            println( "Deployment of ${result.result()} succeeded")
+        }
+        else {
+            println( "Deployment of ${result.result()} failed")
+            vertx.close()
+        }
+    }
 }
