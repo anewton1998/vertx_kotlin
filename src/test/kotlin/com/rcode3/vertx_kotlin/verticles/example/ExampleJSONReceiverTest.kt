294ee4a064512668484ddfd2d4f504a655fd9b68
andy <andy@arin.net>
2018-06-07 03:42:49 -0700

    implemented Junit5 test

--- src/test/kotlin/com/rcode3/vertx_kotlin/verticles/example/ExampleJSONReceiverTest.kt
+++ src/test/kotlin/com/rcode3/vertx_kotlin/verticles/example/ExampleJSONReceiverTest.kt
@@ -3,50 +3,42 @@ package com.rcode3.vertx_kotlin.verticles.example
 
 import com.rcode3.vertx_kotlin.EXAMPLE_JSON_ADDR
 import com.rcode3.vertx_kotlin.JSON_PROP_COUNT
-import io.kotlintest.Description
-import io.kotlintest.Spec
-import io.kotlintest.fail
-import io.kotlintest.shouldBe
-import io.kotlintest.specs.ShouldSpec
 import io.vertx.core.Vertx
 import io.vertx.core.json.JsonObject
+import io.vertx.junit5.VertxExtension
+import io.vertx.junit5.VertxTestContext
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
 
-class ExampleJSONReceiverTest : ShouldSpec() {
 
-    val vertx = Vertx.vertx()
+@DisplayName( "Test of ExampleJSONReceiver" )
+@ExtendWith( VertxExtension::class )
+object ExampleJSONReceiverTest {
 
-    override fun beforeSpec(description: Description, spec: Spec) {
-        vertx.deployVerticle( ExampleJSONReceiver::class.java.name )
+    @BeforeAll
+    @DisplayName( "Deploy ExampleJSONReceiver Verticle" )
+    @JvmStatic
+    fun prepare(vertx: Vertx, testContext: VertxTestContext ) {
+        vertx.deployVerticle( ExampleJSONReceiver::class.java.name,
+                testContext.succeeding{ testContext.completeNow() } )
     }
 
-    override fun afterSpec(description: Description, spec: Spec) {
-        vertx.close()
-    }
-
-    init {
+    @DisplayName( "JSON property is replaced" )
+    @Test
+    fun testJsonProp( vertx: Vertx, testContext: VertxTestContext ) {
 
         val json = JsonObject().putNull( JSON_PROP_COUNT )
-        vertx.eventBus().send<String>( EXAMPLE_JSON_ADDR, json.toString() ) { ar ->
-            if( ar.succeeded() )
-            {
-                should("this test work") {
-                    val jsonResult = JsonObject(ar.result().body())
-                    jsonResult.getInteger(JSON_PROP_COUNT) shouldBe 100
-                }
-            }
-            else {
-                fail( "something bad happened" )
-            }
-        }
-
-/*        should("this test work") {
-            val json = JsonObject().putNull( JSON_PROP_COUNT )
-            vertx.eventBus().send<String>( EXAMPLE_JSON_ADDR, json.toString() ) { ar ->
-                val jsonResult = JsonObject( ar.result().body() )
-                jsonResult.getInteger( JSON_PROP_COUNT ) shouldBe 100
-            }
-        }*/
-
+        vertx.eventBus().send<String>( EXAMPLE_JSON_ADDR, json.toString(),
+                testContext.succeeding{
+                    testContext.verify{
+                        val jsonResult = JsonObject( it.body() )
+                        assertThat( jsonResult.getInteger( JSON_PROP_COUNT) ).isInstanceOf( Integer::class.java )
+                        testContext.completeNow()
+                    }
+                } )
     }
 
 }
