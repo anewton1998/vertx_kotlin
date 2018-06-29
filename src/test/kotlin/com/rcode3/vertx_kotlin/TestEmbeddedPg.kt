079a7a8b50b9b1657fd555182cab2353e92a5d74
andy <andy@arin.net>
2018-06-29 12:32:08 -0400

    added otg embedded postgres

---
+++ src/test/kotlin/com/rcode3/vertx_kotlin/TestEmbeddedPg.kt
@@ -0,0 +1,27 @@
+package com.rcode3.vertx_kotlin
+
+import com.opentable.db.postgres.embedded.EmbeddedPostgres
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+
+@DisplayName( "Embeddeded PostgreSQL")
+object TestEmbeddedPg {
+
+    @DisplayName( "Setting up PostgreSQL" )
+    @Test
+    fun prepare() {
+        val pg = EmbeddedPostgres.start()
+        val ds = pg.getPostgresDatabase()
+        println( "datasource = $ds" )
+        val connection = ds.getConnection()
+        val rs = connection.createStatement().executeQuery( "select 1" )
+        assertThat( rs.next() ).isTrue()
+        assertThat( rs.getInt( 1 ) ).isEqualTo( 1 )
+        assertThat( rs.next() ).isFalse()
+        rs.close()
+        connection.createStatement().execute( "create table users ( name varchar(25) ) ")
+        connection.close()
+    }
+
+}
