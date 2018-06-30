50ecf5302dc4f518763f0dbab6eaab3a3c216b8f
andy <andy@arin.net>
2018-06-30 07:48:50 -0700

    a little refactoring

--- src/main/kotlin/com/rcode3/vertx_kotlin/pg/Cats.kt
+++ src/main/kotlin/com/rcode3/vertx_kotlin/pg/Cats.kt
@@ -2,8 +2,10 @@
 package com.rcode3.vertx_kotlin.pg
 
 import io.reactiverse.reactivex.pgclient.PgConnection
+import io.reactiverse.reactivex.pgclient.Row
 import io.reactivex.Single
 import io.vertx.core.json.JsonArray
+import io.vertx.core.json.JsonObject
 import io.vertx.kotlin.core.json.json
 import io.vertx.kotlin.core.json.obj
 
@@ -17,17 +19,22 @@ class Cats {
                 .map { rowset ->
                     val retval = JsonArray()
                     for( row in rowset ) {
-                        val cat = json {
-                            obj(
-                                    "name" to row.getString( 0 ),
-                                    "type" to row.getString( 1 )
-                            )
-                        }
+                        val cat = jsonObject(row)
                         retval.add( cat )
                         println( "found cat $cat")
                     }
                     retval
                 }
+    }
+
+    private fun jsonObject(row: Row): JsonObject {
+        val cat = json {
+            obj(
+                    "name" to row.getString(0),
+                    "type" to row.getString(1)
+            )
+        }
+        return cat
     }
 
 }
