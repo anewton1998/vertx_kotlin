9d4b62ccf9f5526afd6e91dfc31a2709b1170afb
andy <andy@hxr.us>
2018-06-30 17:35:52 -0700

    add allLimited

--- src/main/kotlin/com/rcode3/vertx_kotlin/pg/CatsDao.kt
+++ src/main/kotlin/com/rcode3/vertx_kotlin/pg/CatsDao.kt
@@ -3,6 +3,7 @@ package com.rcode3.vertx_kotlin.pg
 
 import io.reactiverse.reactivex.pgclient.PgConnection
 import io.reactiverse.reactivex.pgclient.Row
+import io.reactiverse.reactivex.pgclient.Tuple
 import io.reactivex.Single
 import io.vertx.core.json.JsonArray
 import io.vertx.core.json.JsonObject
@@ -11,9 +12,28 @@ import io.vertx.kotlin.core.json.obj
 
 class CatsDao {
 
+    val selectAll = "select name, type from cats"
+    val selectAllLimited = "select name, type from cats limit ?"
+
     fun all( connection: Single<PgConnection>) : Single<JsonArray> {
         return connection.flatMap { conn ->
-            conn.rxQuery( "select name, type from cats" )
+            conn.rxQuery( selectAll )
+                    .doAfterTerminate { conn.close() }
+        }
+                .map { rowset ->
+                    val retval = JsonArray()
+                    for( row in rowset ) {
+                        val cat = jsonObject(row)
+                        retval.add( cat )
+                        println( "found cat $cat")
+                    }
+                    retval
+                }
+    }
+
+    fun allLimited(connection: Single<PgConnection>, limit: Int ) : Single<JsonArray> {
+        return connection.flatMap { conn ->
+            conn.rxPreparedQuery( selectAllLimited, Tuple.of( limit ) )
                     .doAfterTerminate { conn.close() }
         }
                 .map { rowset ->
